25b349eab9136fde2bc38259f774506f4c0d60f3
HerbertWang <wangyong278@gmail.com>
2014-04-19 23:18:55 +1000

    Second Commit

--- Ass1/src/Sensor.java
+++ Ass1/src/Sensor.java
@@ -1,11 +1,100 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Sensor {
 
+	private static String sensorTypeInput, fileNameInput;
+	private String[] values;
+	private String fileName, sensorType, line, value, tempSensorMode, userName;
+	private int period;
+	private FileReader fr;
+	private BufferedReader br;
+	private boolean active;
+	
+	public Sensor(String sensorTypeInput, String fileNameInput){
+		this.sensorType = sensorTypeInput;
+		this.fileName = fileNameInput;
+		try {
+			fr = new FileReader(fileName);
+			br = new BufferedReader(fr);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("the Sensor has been set up");
+	}
+	
+	/**
+	 * This method will activate the sensor
+	 */
+	private void activateSensor(){
+		active = true;
+		Thread sensorTask = new SensorTask();
+		sensorTask.start();
+	}
+	
+	/**
+	 * This class defines the main task that will be executed periodically
+	 */
+	private final class SensorTask extends Thread {
+
+		/**
+		 * This is the main execution of the thread
+		 */
+		public void run() {
+			
+			// if period of last line = 0, then it's time to read the next line
+			if (period <= 0) {
+				// reads the next line. If reached the end of file, start over on the same file
+				try {
+					if((line = br.readLine()) == null) {
+						fr.close();
+						fr = new FileReader(fileName);
+						br = new BufferedReader(fr);
+						line = br.readLine();
+					}
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+						
+				// parse the line. from format, first is value, second field is timeTicker
+				values = line.split(",");
+				value = values[0];
+				System.out.println(value);
+				period = Integer.parseInt(values[1]);
+				System.out.println(period);
+			}
+							
+			// decrement period
+			period--;
+		}
+	}
+	
+	/**
+	 * return sensor type
+	 */
+	public String getSensorType() {
+		return this.sensorType;
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Sensor app=new Sensor("Temperature","Temperature.txt");
+		app.activateSensor();
+		 while(app.active){
+         }
 	}
+	
+	public void exit(){	
+		// exit the entire program
+		System.exit(0);
+	}
+	
 }
--- Ass1/src/UI.java
+++ Ass1/src/UI.java
@@ -5,49 +5,143 @@ import java.io.InputStreamReader;
 
 public class UI {
 
-	public void printMenu() {
-		System.out.println("Welcome to the Smart" +
-				"Home Monitoring System\n"+
-				"Please Select an option\n"+
-				"1. View Log - Temperature" +
-				"adjustment and water usage\n"+
-				"2. View Log - Health " +
-				"warnings for day(s)\n"+
-				"E. exit\n");
+	// initialize variable for further use
+	private String input;
+	private BufferedReader stdin;
+	private String message;
+	private int start,end;
+		
+	public UI() {
+		// load the main menu
+		mainMenu();
+	}
+	
+	private void mainMenu() {
+		stdin = new BufferedReader (new InputStreamReader(System.in));
+		try {
+			System.out.println("Welcome to the Smart Home Monitoring System");
+			System.out.println("Please select an option:");
+			System.out.println("1. View Log - temperature adjustment");
+			System.out.println("2. View Log - Health warnings for day(s)");
+			System.out.println("E. Exit");		
+			System.out.flush();
+			
+			input = stdin.readLine().trim();
+			
+			// based on user input, trigger the function
+			switch(input){
+			case "1": viewLog();
+				break;
+			case "2": viewHealthWarnings();
+				break;
+			case "E": exit();
+				break;
+			case "e": exit();
+				break;
+			default: System.out.println("Invalid command");
+			}
 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// always go back to main menu, the ENTER to return is implemented in each of the functions
+		mainMenu();
 	}
 	
 	
 	/**
+	 * This method executes the UI part of the view temperature log command
+	 */
+	private void viewLog() {
+		//implement retrieve function
+		
+		if(message == null) 
+	   	 {
+             System.out.println("Log of temperature adjustment and water usage is empty");
+	     } else {
+             System.out.println(message);
+	     }
+		
+		// return to the main menu
+		pressEnterToReturn();
+	}
+	
+	/**
+	 * This method executes the UI part of the view temperature log Function
+	 */
+	public void viewHealthWarnings(){
+		
+		String temp=null;;
+		System.out.println("Please enter a start day:");
+        System.out.flush(); // empties buffer, before you input text
+        try {
+			temp = stdin.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        start = Integer.parseInt(temp);
+        
+        System.out.println("Please enter a end day:");
+        System.out.flush(); // empties buffer, before you input text
+        try {
+			temp = stdin.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        end = Integer.parseInt(temp);
+		
+        //implement retrieve function
+        
+        
+        if(message == null) {
+            System.out.println("Log of health warning information is empty");
+        	} else {
+            System.out.println("Health Information of Warnings issued " +
+            		"between <start day> and <end day>:");
+        		}
+		
+		
+     // return to the main menu
+ 		pressEnterToReturn();
+	
+	}
+	
+	/**
+	 * This method will notify the home manager to shut down, remove subscription, close connection
+	 * tells the client to exit, and then exit the whole program itself
+	 */
+	private void exit() {
+		
+		//implement shoutdown function to server
+		
+		System.exit(0);
+	}
+	
+	
+	/**
+	 * This method is to perform the "Return" function after each function has been executed 
+	 */
+	private void pressEnterToReturn() {
+		try {
+			input = stdin.readLine().trim();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// once something is returned, go back to main menu
+		mainMenu();
+	}
+	
+
+	/**
+	 * main method
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		UI app= new UI();
-		do {
-		app.printMenu();
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(System.in));
-		System.out.print("Make selection : ");
-		String Option = in.readLine();
-		if(Option.equals("1"))
-		{
-			
-		}
-		else if(Option.equals("2"))
-		{
-			
-		}
-		else if(Option.equals("E"))
-		{
-			System.exit(1);
-		}
-		else {
-			System.out.println("Invalid command");
-			app.printMenu();
-		}
-		}while(true);
 	}
 
 }
